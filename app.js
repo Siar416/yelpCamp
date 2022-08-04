8254c969674bc3bac82e51b44177b56f3f69e085
Siar Wahidi <siar.wahidi@gmail.com>
2022-08-04 09:39:19 -0400

    created a views dir, along with a home ejs file that will be rendered

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require("path");
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-  res.send("Yelp Camp");
+  res.render("home");
 });
 
 app.listen(PORT, () => {
