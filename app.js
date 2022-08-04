f152157e97195df0bff5f38ff99305fdb22a06eb
Siar Wahidi <siar.wahidi@gmail.com>
2022-08-04 09:32:57 -0400

    setup first basic route

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const path = require("path");
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Yelp Camp");
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on PORT: ${PORT}`);
 });
