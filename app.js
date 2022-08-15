adf01aecf1eb1bf59b6b2b726e2f0aa96588030b
Siar Wahidi <siar.wahidi@gmail.com>
2022-08-15 14:30:22 -0400

    Installed express session and configured

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const session = require("express-session");
 const ExpressError = require("./utils/ExpressError");
 const methodOverrid = require("method-override");
 const PORT = 3000;
@@ -33,6 +34,18 @@ app.use(
   })
 );
 app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+  secret: "thisshouldbeabettersecret!",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
 
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
