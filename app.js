1c0db436b06b650dc34229a479a3ac5d6be7fbfc
Siar Wahidi <siar.wahidi@gmail.com>
2022-08-15 13:23:14 -0400

    moved campgrounds routes to routes folder

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const Review = require("./models/review");
 const methodOverrid = require("method-override");
 const PORT = 3000;
 
+const campgrounds = require("./routes/campgrounds");
+
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -34,16 +36,6 @@ app.use(
   })
 );
 
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next(error);
-  }
-};
-
 const validateReview = (req, res, next) => {
   const { error } = reviewSchema.validate(req.body);
   console.log(error);
@@ -55,78 +47,12 @@ const validateReview = (req, res, next) => {
   }
 };
 
+app.use("/campgrounds", campgrounds);
+
 // base route
 app.get("/", (req, res) => {
   res.render("home");
 });
-
-// route to get all campgrounds
-app.get(
-  "/campgrounds",
-  catchAsync(async (req, res) => {
-    const campgrounds = await Campgound.find({});
-    res.render("campgrounds/index", { campgrounds });
-  })
-);
-
-// route to show form for adding new campground
-app.get("/campgrounds/new", (req, res) => {
-  res.render("campgrounds/new");
-});
-
-// route to post new campground
-app.post(
-  "/campgrounds",
-  validateCampground,
-  catchAsync(async (req, res, next) => {
-    // if (!req.body.campground) {
-    //   throw new ExpressError("Invalid Campground Data", 400);
-    // }
-    const campground = new Campgound(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-// route for single campground based on id
-app.get(
-  "/campgrounds/:id",
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campgound.findById(id).populate("reviews");
-    res.render("campgrounds/show", { campground });
-  })
-);
-
-// route to serve up form to edit campground
-app.get("/campgrounds/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campgound.findById(id);
-  res.render("campgrounds/edit", { campground });
-});
-
-// route that updates the campground
-app.put(
-  "/campgrounds/:id",
-  validateCampground,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campgound.findByIdAndUpdate(id, {
-      ...req.body.campground,
-    });
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-// route to delete campground
-app.delete(
-  "/campgrounds/:id",
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campgound.findByIdAndDelete(id);
-    res.redirect("/campgrounds");
-  })
-);
 
 // route to create review for campground
 app.post(
