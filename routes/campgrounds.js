01d31ef0eb5a635c4dd97aff13acd41310b921b7
Siar Wahidi <siar.wahidi@gmail.com>
2022-08-15 14:40:01 -0400

    Installed connect flash to display messages

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -37,8 +37,10 @@ router.post(
     // if (!req.body.campground) {
     //   throw new ExpressError("Invalid Campground Data", 400);
     // }
+
     const campground = new Campgound(req.body.campground);
     await campground.save();
+    req.flash("success", "Successfully made a new campground");
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
